1aed136861bfcf84420e8aa5761e8e056ce8da9c
poojathote1991 <poojaghode6@gmail.com>
2023-07-07 23:35:21 +0530

    config file add

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+PORT_NUMBER=8080
+MOGODB_PORT_NUMBER=27017
+MODEL_FILE_PATH=os.path.join( r'static','model_file','medical_insurance.pkl')
+JSON_FILE_PATH=os.path.join(r'static','json_file','proj_data.json')
